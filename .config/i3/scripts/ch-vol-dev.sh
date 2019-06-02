e5f966d202af022b737bfd9b7aaae5e94feb13fe
Zichen Liu <btstream@hotmail.com>
2019-06-02 17:30:05 +0800

    add scripts to change default audio defice

---
+++ .config/i3/scripts/ch-vol-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+info_file=/tmp/pulse_devices
+pacmd list-sinks > $info_file
+new_sink=$(cat $info_file | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
+echo $new_sink
+device_name=$(cat $info_file | grep alsa.card_name | cut -d\= -f2 | sed "s/\"//g; s/^ *//g; ${new_sink}!d")
+
+echo $device_name
+notify-send -a "Audio Mixer" "Setting default device to: $device_name";
+pacmd set-default-sink $new_sink
+
+pacmd list-sink-inputs | grep index | while read line
+do
+    echo "Moving input: ";
+    echo $line | cut -f2 -d' ';
+    echo "to sink: $new_sink";
+    pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
+done
