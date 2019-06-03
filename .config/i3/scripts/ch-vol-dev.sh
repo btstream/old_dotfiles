da981c7a0bdf05b89049e2e6c0cde8169ed37464
Zichen Liu <btstream@hotmail.com>
2019-06-04 00:51:15 +0800

    modify xprofile's xdg current session config

--- .config/i3/scripts/ch-vol-dev.sh
+++ .config/i3/scripts/ch-vol-dev.sh
@@ -3,8 +3,9 @@
 info_file=/tmp/pulse_devices
 pacmd list-sinks > $info_file
 new_sink=$(cat $info_file | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
+index=$((new_sink+1))
 echo $new_sink
-device_name=$(cat $info_file | grep alsa.card_name | cut -d\= -f2 | sed "s/\"//g; s/^ *//g; ${new_sink}!d")
+device_name=$(cat $info_file | grep alsa.card_name | cut -d\= -f2 | sed "s/\"//g; s/^ *//g; ${index}!d")
 
 echo $device_name
 notify-send -a "Audio Mixer" "Setting default device to: $device_name";
