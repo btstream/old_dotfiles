15e1a2f7d5ea7afa6530c12619c35c4f152c4acd
Zichen Liu <btstream@hotmail.com>
2019-06-05 00:09:54 +0800

    use temp file to save device infomation

--- .config/i3/scripts/ch-vol-dev.sh
+++ .config/i3/scripts/ch-vol-dev.sh
@@ -6,7 +6,8 @@ declare -a sink_index=()
 declare -A dev_map=()
 
 # cache value
-info_file=/tmp/pulse_devices
+info_file=$(mktemp --suffix '-chdadv')
+device_name_file=$(mktemp --suffix 'chdadv')
 pacmd list-sinks > $info_file
 
 # get indexes
@@ -18,14 +19,15 @@ for i in $indexes; do
 done
 
 # get device names
-cat $info_file | grep alsa.card_name | cut -d\= -f2 | sed 's/"//g; s/^ *//g' > /tmp/pulse_device_name 
+cat $info_file | grep alsa.card_name | cut -d\= -f2 | sed 's/"//g; s/^ *//g' > $device_name_file
 
+# generate devcice map
 k=0
 for i in ${sink_index[@]}; do
     l=$((k+1))
-    device_name=$(sed "$l!d" /tmp/pulse_device_name)
+    device_name=$(sed "$l!d" $device_name_file)
     dev_map[${sink_index[$k]}]="$device_name"
-    k=$((k+1))
+    k=$l
 done
 
 new_sink=$(cat $info_file | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
@@ -39,3 +41,5 @@ do
     echo "to sink: $new_sink";
     pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
 done
+
+rm -rf $info_file $device_name_file 
