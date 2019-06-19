dd431407e1ed5716a9429157c5b758ec5518fbe1
Zichen Liu <btstream@hotmail.com>
2019-06-19 23:07:52 +0800

    use card name for audio device selection in pulaseaudio-rofi.sh

--- .config/polybar/scripts/pulseaudio-rofi.sh
+++ .config/polybar/scripts/pulseaudio-rofi.sh
@@ -1,16 +1,57 @@
 #!/bin/sh
 
 outputs() {
-    OUTPUT=$(pactl list short sinks | cut  -f 2 | rofi -dmenu -p "OUTPUT" -mesg "Select prefered output audio device" )
-    pacmd set-default-sink "$OUTPUT" >/dev/null 2>&1
+    declare -a sink_index=()
 
-    for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}'); do
-        pacmd move-sink-input "$playing" "$OUTPUT" >/dev/null 2>&1
+    # declare a value maps
+    declare -A dev_map=()
+
+    # cache value
+    info_file=$(mktemp --suffix '-chdadv')
+    device_name_file=$(mktemp --suffix '-chdadv')
+    pacmd list-sinks > $info_file
+
+    # get indexes
+    indexes=$(cat $info_file | grep index | cut -d\: -f2 | sed 's/ *//g')
+    j=0
+    for i in $indexes; do
+        sink_index[$j]=$i
+        j=$((j+1))
     done
+
+    # get device names
+    cat $info_file | grep alsa.card_name | cut -d\= -f2 | sed 's/"//g; s/^ *//g' > $device_name_file
+
+    # generate devcice map
+    k=0
+    for i in ${sink_index[@]}; do
+        l=$((k+1))
+        device_name=$(sed "$l!d" $device_name_file)
+        dev_map[$device_name]=${sink_index[$k]}
+        k=$l
+    done
+
+    OUTPUT=$(cat $device_name_file | rofi -dmenu -i -p "OUTPUT" -mesg "Select prefered output audio device")
+    if [[ x$OUTPUT != x ]]; then
+        new_sink=${dev_map[$OUTPUT]}
+        echo $new_sink
+        pacmd set-default-sink $new_sink
+
+        pacmd list-sink-inputs | grep index | while read line
+        do
+            echo "Moving input: ";
+            echo $line | cut -f2 -d' ';
+            echo "to sink: $new_sink";
+            pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
+        done
+        notify-send -a "Audio Mixer" "Setting default device to: $OUTPUT"
+    fi
+
+    rm -rf $info_file $device_name_file 
 }
 
 inputs() {
-    INPUT=$(pactl list short sources | cut  -f 2 | grep input | rofi -dmenu -p "Input" -mesg "Select prefered input source" )
+    INPUT=$(pactl list short sources | cut  -f 2 | grep input | rofi -i -dmenu -p "INPUT" -mesg "Select prefered input source" )
     pacmd set-default-source "$INPUT" >/dev/null 2>&1
 
     for recording in $(pacmd list-source-outputs | awk '$1 == "index:" {print $2}'); do
