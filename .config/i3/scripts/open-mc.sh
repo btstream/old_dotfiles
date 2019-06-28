8d8a480b9a1726bf3ba8489cb16acde5cd832c6a
Zichen Liu <btstream@hotmail.com>
2019-06-28 22:25:37 +0800

    modify default open mc action

--- .config/i3/scripts/open-mc.sh
+++ .config/i3/scripts/open-mc.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-i3-msg 'workspace 4:FM; exec urxvtc -e mc'
+
+new_mc_window() {
+    i3-msg 'workspace 4:FM; exec urxvtc -e mc'
+}
+
+i=0
+ws4index=-1
+for workspace in $(i3-msg -t get_workspaces | jq '.[] | .num'); do
+    if [[ $workspace == 4 ]]; then
+        ws4index=$i
+        break;
+    fi
+    i=$((i+1))
+done
+
+mc_window_id=$(xdotool search --desktop $ws4index --name "^mc")
+if [[ x$mc_window_id == "x" ]]; then
+    new_mc_window
+else
+    i3-msg 'workspace 4:FM'
+fi
+
